91c5e84d958a75c1cbffca3bcee9846f7ef9e2d0
Sergio De Luca <sdlbsso@gmail.com>
2023-01-07 14:28:31 -0300

    configuracion de swagger para jwt

--- WebApiAutores/StartUp.cs
+++ WebApiAutores/StartUp.cs
@@ -43,6 +43,32 @@ namespace WebApiAutores
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiAutores", Version = "v1" });
+                c.AddSecurityDefinition("Bearer ", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                });
+
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[]{ }
+                    }
+                });
+
+
             });
             services.AddAutoMapper(typeof(StartUp));
 
